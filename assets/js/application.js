82be94403a94607b8570ae79bd518fa5d7c4dd80
astrum0 <acc2for2all@gmail.com>
2025-02-06 17:42:23 +0300

    Сверстал 4ую секцию

--- assets/js/application.js
+++ assets/js/application.js
@@ -9,15 +9,4 @@ document.addEventListener('DOMContentLoaded', function () {
       pagination: false,  
       gap: '25px',
     }).mount();
-  
-    new Splide('#image-sliderr', {
-      type: 'loop',      
-      perPage: 2,         
-      autoplay: true,   
-      interval: 3000,     
-      pauseOnHover: true, 
-      arrows: false,      
-      pagination: false,  
-      gap: '25px',
-    }).mount();
   });
