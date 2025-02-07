653a50f889a67b6863c97338c715dbcefae9ca35
astrum0 <acc2for2all@gmail.com>
2025-02-07 16:33:33 +0300

    Добавил иконку для input в footer и для input в service

--- assets/js/application.js
+++ assets/js/application.js
@@ -9,26 +9,5 @@ document.addEventListener('DOMContentLoaded', function () {
       pagination: false,  
       gap: '25px',
     }).mount();
-  });
-    
-    const collapsibles = document.querySelectorAll(".faq__title-wrapper");
-    collapsibles.forEach(button => {
-      button.addEventListener("click", function () {
-         this.classList.toggle("active");
-         const content = this.nextElementSibling;
-         if (content.style.height === "auto" ) {
-             content.style.height = 0 ;
-         } else {
-             content.style.height = "auto";
-         }
-     });
 
-  //    $( ".faq__item" ).click(function() {
-  //     if (  $( this ).css( "faq__icon-more" ) == 'none' ){
-  //         $(this).css("transform","rotate(180deg)");
-  //     } else {
-  //         $(this).css("transform","" );
-  //     }
-  // });
-  
 });
