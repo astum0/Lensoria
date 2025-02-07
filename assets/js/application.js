f3d036627e6c6affd1121f8f1413a3e3b2b6f9e8
astrum0 <acc2for2all@gmail.com>
2025-02-07 03:49:17 +0300

    Доделал восьмую секцию, 3 часа потратил на баги с dropdown menu и все равно не работает

--- assets/js/application.js
+++ assets/js/application.js
@@ -10,3 +10,25 @@ document.addEventListener('DOMContentLoaded', function () {
       gap: '25px',
     }).mount();
   });
+    
+    const collapsibles = document.querySelectorAll(".faq__title-wrapper");
+    collapsibles.forEach(button => {
+      button.addEventListener("click", function () {
+         this.classList.toggle("active");
+         const content = this.nextElementSibling;
+         if (content.style.height === "auto" ) {
+             content.style.height = 0 ;
+         } else {
+             content.style.height = "auto";
+         }
+     });
+
+     $( ".faq__item" ).click(function() {
+      if (  $( this ).css( "faq__icon-more" ) == 'none' ){
+          $(this).css("transform","rotate(180deg)");
+      } else {
+          $(this).css("transform","" );
+      }
+  });
+  
+});
