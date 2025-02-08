6e61b865ff3939c6a908ba15ba58f3dede6525f7
astrum0 <acc2for2all@gmail.com>
2025-02-08 09:36:03 +0300

    Доверстал страницу About Us

--- assets/js/application.js
+++ assets/js/application.js
@@ -10,4 +10,14 @@ document.addEventListener('DOMContentLoaded', function () {
       gap: '25px',
     }).mount();
 
+    document.querySelectorAll('.faq__item').forEach(item => {
+      item.addEventListener('click', function(e) {
+          if (e.target.closest('.faq__button-more')) return;
+          this.classList.toggle('active');
+      });
+      item.querySelector('.faq__button-more').addEventListener('click', function(e) {
+          e.stopPropagation();
+          item.classList.toggle('active');
+      });
+  });
 });
